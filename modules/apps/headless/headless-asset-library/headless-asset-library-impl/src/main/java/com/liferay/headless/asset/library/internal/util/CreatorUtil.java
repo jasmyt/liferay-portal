3e19cae5d5834b1cd8385f4a41de51436d19fc02
Jason Myatt <jason.myatt@liferay.com>
2026-03-19 13:40:32 -0400

    LPD-83063 Use module specific CreatorUtil

---
+++ modules/apps/headless/headless-asset-library/headless-asset-library-impl/src/main/java/com/liferay/headless/asset/library/internal/util/CreatorUtil.java
@@ -0,0 +1,60 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.headless.asset.library.internal.util;
+
+
+
+/**
+ * @author Jason Myatt
+ */
+public class CreatorUtil {
+
+	public static Creator toCreator(Portal portal, User user) {
+		if ((user == null) || user.isGuestUser()) {
+			return null;
+		}
+
+		return new Creator() {
+			{
+				setAdditionalName(user::getMiddleName);
+				setContentType(() -> "UserAccount");
+				setExternalReferenceCode(user::getExternalReferenceCode);
+				setFamilyName(user::getLastName);
+				setGivenName(user::getFirstName);
+				setId(user::getUserId);
+				setImage(
+					() -> {
+						if (user.getPortraitId() == 0) {
+							return null;
+						}
+
+						ThemeDisplay themeDisplay = new ThemeDisplay() {
+							{
+								setPathImage(portal.getPathImage());
+							}
+						};
+
+						return user.getPortraitURL(themeDisplay);
+					});
+				setName(user::getFullName);
+				setProfileURL(
+					() -> {
+						Group group = user.getGroup();
+
+						ThemeDisplay themeDisplay = new ThemeDisplay() {
+							{
+								setPortalURL(StringPool.BLANK);
+								setSiteGroupId(group.getGroupId());
+							}
+						};
+
+						return group.getDisplayURL(themeDisplay);
+					});
+			}
+		};
+	}
+
+}
